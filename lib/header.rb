8b89c870b9a2f6d7ea989e7c7d0adac81134863e
Rafik Moreira <rafikmoreiradias@outlook.com>
2022-01-16 19:18:04 -0300

    Create header.rb

---
+++ lib/header.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# titles
+class Header
+  def show_title(title)
+    puts '======================================================'
+    puts title
+    puts '======================================================'
+  end
+end
